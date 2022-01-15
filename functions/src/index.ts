208894153a0f03a6985c052bb804179144aabbb9
Arthur G <lapteuh@gmail.com>
2022-01-15 19:43:37 +0500

    Add some firebase function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,16 @@
 import * as functions from "firebase-functions";
+import { isLeft } from "fp-ts/lib/Either";
+import { validatePirojok } from "pirojok";
 
 export const helloWorld = functions.https.onRequest((request, response) => {
+  const pirojok = validatePirojok(request.body);
+
+  if (isLeft(pirojok)) {
+    response.status(400).json(pirojok.left);
+    functions.logger.error("Hello errors!", { errors: pirojok.left });
+    return;
+  }
+
   functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+  response.json("Hello from Firebase!");
 });
